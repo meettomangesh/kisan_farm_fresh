dd49ba201d041315ac4daec9c39bb4381a7de38c
mangeshn <>
2021-03-06 00:04:42 +0530

    To add postcode API change

--- database/migrations/2020_12_05_194436_create_sp_search_pincode.php
+++ database/migrations/2020_12_05_194436_create_sp_search_pincode.php
@@ -18,23 +18,41 @@ class CreateSpSearchPincode extends Migration
         searchPincode:BEGIN 
             IF pinCode IS NULL OR pinCode = '' THEN 
                 SELECT
-                id,
-                    pin_code AS pc
+                pin_codes.id,
+                pin_code AS pc,
+                pin_codes.country_id,
+                pin_codes.state_id,
+                pin_codes.city_id,
+                cities.name AS city_name,
+                states.name AS state_name,
+                countries.name AS country_name
                 FROM
                     pin_codes
+                JOIN cities ON cities.id=pin_codes.city_id
+                JOIN states ON states.id=pin_codes.state_id
+                JOIN countries ON countries.id=pin_codes.country_id
                 WHERE
-                    status = 1 
+                    pin_codes.status = 1 
                 ORDER BY id;
             ELSE 
                 SELECT
-                id,
-                    pin_code AS pc
+                pin_codes.id,
+                pin_code AS pc,
+                pin_codes.country_id,
+                pin_codes.state_id,
+                pin_codes.city_id,
+                cities.name AS city_name,
+                states.name AS state_name,
+                countries.name AS country_name
                 FROM
                     pin_codes
+                JOIN cities ON cities.id=pin_codes.city_id
+                JOIN states ON states.id=pin_codes.state_id
+                JOIN countries ON countries.id=pin_codes.country_id
                 WHERE
-                    status = 1 
+                    pin_codes.status = 1        
                 AND
-                    pin_code LIKE CONCAT(pinCode, '%')
+                pin_codes.pin_code LIKE CONCAT(pinCode, '%')
                 ORDER BY id;
             END IF;
 

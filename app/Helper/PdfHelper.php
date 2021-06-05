f191815349159f4de022b38e39b2acb4764399f0
Mangesh Navale <you@example.com>
2021-06-06 00:02:29 +0530

    To save work

--- app/Helper/PdfHelper.php
+++ app/Helper/PdfHelper.php
@@ -25,7 +25,7 @@ class PdfHelper
      * To Generate PDF
      *
      */
-    public static function generatePDF($html, $filePath, $fileName,$details=[])
+    public static function generatePDF($html, $filePath, $fileName, $details = [])
     {
         try {
 
@@ -35,27 +35,27 @@ class PdfHelper
             $filename = $fileName;
             $finalFileNameWithPath = $filepath . $filename . '.pdf';
             $path = '';
-            PDF::setOptions(['isRemoteEnabled' => true])->loadHTML($html)->setPaper('a4', 'landscape')->setWarnings(false)->save($finalFileNameWithPath);          
+            PDF::setOptions(['isRemoteEnabled' => true])->loadHTML($html)->setPaper('a4', 'landscape')->setWarnings(false)->save($finalFileNameWithPath);
             if (file_exists($finalFileNameWithPath)) {
                 //send mail of newly generated quotation pdf.
-                $path = self::uploadInvoiceToS3($finalFileNameWithPath,$details);
-
+                $path = self::uploadInvoiceToS3($finalFileNameWithPath, $details);
+                exit;
             } else {
                 $path = ''; //not generated successfully  
             }
         } catch (Exception $e) {
-            $path = ''; 
+            $path = '';
         }
         return $path;
-    }   
+    }
 
 
-    public static function uploadInvoiceToS3($finalFileNameWithPath,$details)
-    {
+    public static function uploadInvoiceToS3($finalFileNameWithPath, $details)
+    {   
         //create s3 object
         //$s3 = App::make('aws')->createClient('s3');
         $s3 = Storage::disk('s3');
-        $path = $details['user_id']."/";
+        $path = $details['user_id'] . "/";
         $final_url = $path . $details['order_id'] . '.pdf';
         // $path = self::getBrandImageUploadFolder();
         // $milliseconds = round(microtime(true) * 1000);
@@ -75,7 +75,15 @@ class PdfHelper
 
             // $s3->put($fileNameMedium, file_get_contents('/tmp/medium-brand' . $milliseconds . $fileExtension), 'public');
 
-            $s3->put($final_url, file_get_contents($finalFileNameWithPath), 'public');
+            // $s3->put($final_url, file_get_contents($finalFileNameWithPath), 'public');
+            $result = $s3->putObject([
+                'Bucket' => 'kff-invoice-dev',
+                'Key'    => $details['order_id'] . '.pdf',
+                'Body'   => $finalFileNameWithPath,
+                'ACL'    => 'public-read'
+            ]);
+            print_r($result);
+            echo $result['ObjectURL'] . PHP_EOL; exit;
         } catch (Aws\Exception\S3Exception $e) {
             echo "There was an error uploading the file.\n";
         }

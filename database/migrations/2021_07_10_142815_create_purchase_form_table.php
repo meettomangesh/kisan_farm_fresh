2c0f4b04fca0699067ded05275603390ee4295e9
Mangesh Navale <you@example.com>
2021-06-12 15:08:24 +0530

    To save work

--- database/migrations/2021_07_10_142815_create_purchase_form_table.php
+++ database/migrations/2021_07_10_142815_create_purchase_form_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePurchaeFormTable extends Migration
+class CreatePurchaseFormTable extends Migration
 {
     /**
      * Run the migrations.

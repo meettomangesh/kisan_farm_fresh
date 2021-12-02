e0f39a222759d022c999fe1682f7f8d77b54a015
Akshay N <appdevise@gmail.com>
2021-12-02 21:16:27 +0530

    Changes

--- database/migrations/2021_04_18_194436_create_sp_get_promo_codes.php
+++ database/migrations/2021_04_18_194436_create_sp_get_promo_codes.php
@@ -41,7 +41,8 @@ class CreateSpGetPromoCodes extends Migration
             ELSE '' END AS promo_code_value
             FROM promo_codes AS pc
             JOIN promo_code_master AS pcm ON pcm.id = pc.promo_code_master_id
-            WHERE pc.user_id = userId AND pc.is_code_used = 0 AND pc.status = 1 AND pc.start_date <= CURDATE() AND pc.end_date >= CURDATE()
+            JOIN users AS u ON u.id = pc.user_id
+            WHERE pc.user_id = userId AND pc.is_code_used = 0 AND pc.status = 1 AND pc.start_date <= CURDATE() AND pc.end_date >= CURDATE() AND u.status = 1
             ORDER BY pc.id DESC
             LIMIT noOfRecords
             OFFSET pageNumber;

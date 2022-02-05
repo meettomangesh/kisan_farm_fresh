8996f7c49c06e0b74aa42cb7144e9f77eca8528c
mangeshn@iprogrammer.com <mangeshn@iprogrammer.com>
2022-02-06 01:03:29 +0530

    To add error object filter

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -10,9 +10,10 @@ use Illuminate\Http\Response;
 use PDO;
 use Redis;
 use DB;
+
 class BaseController extends Controller
 {
-    
+
     /**
      * All the required libraries will be instantiated in these variables
      * @var object
@@ -39,21 +40,23 @@ class BaseController extends Controller
     const INVENTORY_RESTRICTION_FAILURE_RESPONSE_CODE = 108;
     //SMS transaction type
     const SMS_MSG_TEMPLATES = [201 => "APP_REGISTER_OTP", 202 => "APP_LOGIN_OTP", 203 => "APP_RESET_PASSWORD", 204 => "APP_FORGET_PASSWORD"];
-    CONST VALIDATION_ERROR = 'Validation Error.';
-        /**
+    const VALIDATION_ERROR = 'Validation Error.';
+    /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(Request $request, Response $response
-    //, PDO $pdo
-    , Redis $redis
+    public function __construct(
+        Request $request,
+        Response $response
+        //, PDO $pdo
+        ,
+        Redis $redis
     ) {
         $this->request = $request;
         $this->response = $response;
         $this->pdo = DB::connection()->getPdo();
         $this->redis = $redis;
-       
     }
 
 
@@ -64,7 +67,7 @@ class BaseController extends Controller
      */
     public function sendResponse($result, $message)
     {
-    	$response = [
+        $response = [
             'success' => true,
             'data'    => $result,
             'message' => $message,
@@ -80,19 +83,21 @@ class BaseController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function sendError($error, $errorMessages = [], $code = 404,$result = [])
+    public function sendError($error, $errorMessages = [], $code = 404, $result = [])
     {
-        if(!isset($result['message'])) {
+        if (!isset($result['message'])) {
             $result['message'] = "";
         }
-    	$response = [
+        $response = [
             'success' => false,
             'data'    => $result,
             'message' => $error,
         ];
-        if(!empty($errorMessages)){
-            if(!isset($errorMessages['message'])) {
-                $errorMessages['message'] = "";
+        if (!empty($errorMessages)) {
+            if (is_array($errorMessages)) {
+                if (!isset($errorMessages['message'])) {
+                    $errorMessages['message'] = "";
+                }
             }
             $response['data'] = $errorMessages;
         }
